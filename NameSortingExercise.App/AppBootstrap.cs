63df22a0ae844cc6ebf89fb5a11b2d9f58e04fff
Afnan Bashir <2023923+afnan@users.noreply.github.com>
2025-08-24 13:29:19 +1000

    re create console app in .net 9

---
+++ NameSortingExercise.App/AppBootstrap.cs
@@ -0,0 +1,6 @@
+﻿
+namespace NameSortingExercise.App;
+    internal class AppBootstrap
+    {
+    }
+
--- NameSortingExercise.App/Program.cs
+++ NameSortingExercise.App/Program.cs
@@ -1,17 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NameSortingExercise
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Error.WriteLine("Usage: name-sorter <path-to-unsorted-names-list.txt>");
-            
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");

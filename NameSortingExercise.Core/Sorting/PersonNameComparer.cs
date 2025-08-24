ae6921c95bf832b1d422f9180f598334a46f510c
Afnan Bashir <2023923+afnan@users.noreply.github.com>
2025-08-24 13:06:02 +1000

    test(sort): comparer rules (red)

---
+++ NameSortingExercise.Core/Sorting/PersonNameComparer.cs
@@ -0,0 +1,13 @@
+﻿
+using NameSortingExercise.Core.Domain;
+
+namespace NameSortingExercise.Core.Sorting
+{
+    public class PersonNameComparer : IComparer<Person>
+    {
+        public int Compare(Person? x, Person? y)
+        {
+            return 0; // stub so tests compile and then fail (red)
+        }
+    }
+}
